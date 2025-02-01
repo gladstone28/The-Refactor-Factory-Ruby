c5b20df3ec53dda3990252f8f341fa857a2ee029
Gladstone Rose <gladstonerose@hotmail.com>
2025-02-01 17:41:24 -0500

    add new folder and files

---
+++ BLOCKS-PROCS-AND-LAMDAS/i-am-a-block.rb
@@ -0,0 +1,9 @@
+=begin
+1.Let’s do a little review! Use .times and a block to puts the string “I’m a block!” five times
+=end
+
+# Write your code below!
+
+5.times do 
+  puts "I'm a block!"
+end
---
+++ BLOCKS-PROCS-AND-LAMDAS/you-know-this.rb
@@ -0,0 +1,7 @@
+[1, 2, 3].each do |num|
+  puts num
+end
+# ==> Prints 1, 2, 3 on separate lines
+
+[1, 2, 3].each { |num| puts num }
+# ==> Prints 1, 2, 3 on separate lines
